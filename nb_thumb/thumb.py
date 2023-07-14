f19a7b2323a23fe6be1f992e829854f792760b72
Hamel Husain <hamel.husain@gmail.com>
2023-07-13 23:00:59 -0700

    save

--- nb_thumb/thumb.py
+++ nb_thumb/thumb.py
@@ -7,14 +7,16 @@ __all__ = ['get_img', 'img2thumb', 'nb2thumb']
 import io, base64, re
 import nbformat
 from PIL import Image
-from PIL.PngImagePlugin import PngImageFile
 from fastcore.foundation import L
 from fastcore.test import test_fail
 from io import BytesIO
 
 # %% ../nbs/00_thumb.ipynb 4
 def _decode_img(base64_str):
-    return Image.open(BytesIO(base64.b64decode(base64_str)))
+    img = Image.open(BytesIO(base64.b64decode(base64_str)))
+    if img.mode == "RGBA":
+        img = img.convert("RGB")
+    return img
 
 def get_img(nb_path, label='thumbnail'):
     "Get image from notebook with a cell comment #|label: {label}"
@@ -35,14 +37,14 @@ def get_img(nb_path, label='thumbnail'):
         raise Exception(f'{nb_path} does not contain a cell with `#|label: {label}`')
 
 # %% ../nbs/00_thumb.ipynb 9
-def img2thumb(img:PngImageFile, size=(260,260)):
+def img2thumb(img:Image.Image, size=(260,260)):
     "Convert image to thumbnail."
     thumb_size = size[0] * 2, size[1] * 2
     img.thumbnail(thumb_size)
     return img
 
 # %% ../nbs/00_thumb.ipynb 12
-def nb2thumb(nb_path, label='thumbnail', size=(260,260)) -> PngImageFile:
+def nb2thumb(nb_path, label='thumbnail', size=(260,260)) -> Image.Image:
     "Extract thumbnail corresponding to the cell with the comment `#|label: {label}` from a notebook."
     img = get_img(nb_path=nb_path, label=label)
     return img2thumb(img, size=size)
